f21d30e4d8929958b85f1415a6c705a70875b93d
Mohitsen11 <mohitsen0102@gmail.com>
2023-10-10 05:42:49 +0530

    Created a new route for getting userDetails

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,13 +2,14 @@
 import express from 'express';
 
 // const { signin , signup } = require('../controllers/authController');
-import { signin, signup } from '../controllers/authController.js';
+import { getUserDetails, signin, signup } from '../controllers/authController.js';
+import jwtAuth from '../middleware/jwtAuth.js';
 
 const authRouter = express.Router();
 
 authRouter.post('/signup' ,  signup);
 authRouter.post('/signin' , signin);
-
+authRouter.get('/me' , jwtAuth, getUserDetails);
 
 
 export default authRouter;
