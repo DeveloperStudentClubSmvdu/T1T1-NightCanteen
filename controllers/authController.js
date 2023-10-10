a5a73c03bd20871142764617c447c5642d887fa0
Mohitsen11 <mohitsen0102@gmail.com>
2023-10-10 06:09:28 +0530

    Removed unnecessary comments

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,49 +14,48 @@ const signup = async (req , res, next) => {
          console.log( username , email , password , confirmPassword);
 
          try{
-                // Checking every field 
+                
                 if(!username || !email || !password || !confirmPassword){
                     return next(new AppError("Every field is required" , 400));
                 }
                
-                // Matching the password and confirmPassword
+                
                 if(password !== confirmPassword){
                     return next(new AppError("Password and ConfirmPassword don't match", 400));
                 }
 
-                // Here validating the email 
+           
                 const validEmail = emailValidator.validate(email);
 
-                // when given email is not valid
+              
                 if(!validEmail){
                     return next(new AppError("Email is not valid", 400));
                 }
 
-                // Cheking the user that if it is already a user or not 
+                
                 const checkUser = await User.findOne({email});
 
                 if(checkUser){
                     return next(new AppError("Email already exists" , 400));
                 }
 
-                // if given info is new ( Means he/she is a new user) so save their data into database
+               
                 const user = await User.create({
                     username,
                     email,
                     password
                 });
 
-                // if somehow , app failed to register the user then give a message as error
+                
                 if(!User){
                     return next(new AppError("User registration faild, please try again" , 400));
                 }
 
-                // now save the user
+            
                 await user.save();
 
                 // const token = await user.jwtToken();
 
-                // hide the user password as we are going to show the user details when user registered successfully
                 user.password = undefined;
 
                 // res.cookie("token" , token , cookieOption);
@@ -68,7 +67,7 @@ const signup = async (req , res, next) => {
                 });
 
          } catch (error) {
-            // when an already register user tried to register
+            
             if( error.code === 11000){
                 return res.status(200).json({
                     success : false,
